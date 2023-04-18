7a588c312a3cf9f71a2c483bb58c5da01b7dffb7
Bruno Bordin <bruno.bordin@hammerconsult.com.br>
2023-04-18 15:28:38 -0300

    Add project files.

---
+++ CleanArchMvc.Application/Services/ProductService.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Products.Queries;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IMediator _mediator;
+        private readonly IMapper _mapper;
+
+        public ProductService(IMapper mapper, IMediator mediator)
+        {
+            _mediator = mediator;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetProducts()
+        {
+            //var productsEntity = await _productRepository.GetProductsAsync();
+            //return _mapper.Map<IEnumerable<ProductDTO>>(productsEntity);
+            var productsQuery = new GetProductsQuery();
+
+            if (productsQuery == null)
+                throw new Exception($"entity could not be loaded");
+
+            var result = await _mediator.Send(productsQuery);
+
+            return _mapper.Map<IEnumerable<ProductDTO>>(result);
+        }
+
+        //public async Task<ProductDTO> GetById(int? id)
+        //{
+        //    var productEntity = await _productRepository.GetByIdAsync(id);
+        //    return _mapper.Map<ProductDTO>(productEntity);
+        //}
+
+        //public async Task<ProductDTO> GetProductCategory(int? id)
+        //{
+        //    var productEntity = await _productRepository.GetProductCategoryAsync(id);
+        //    return _mapper.Map<ProductDTO>(productEntity);
+        //}
+
+        //public async Task Add(ProductDTO productDto)
+        //{
+        //    var productEntity = _mapper.Map<Product>(productDto);
+        //    await _productRepository.CreateAsync(productEntity);
+        //}
+
+        //public async Task Update(ProductDTO productDto)
+        //{
+        //    var productEntity = _mapper.Map<Product>(productDto);
+        //    await _productRepository.UpdateAsync(productEntity);
+        //}
+
+        //public async Task Remove(int? id)
+        //{
+        //    var productEntity = _productRepository.GetByIdAsync(id).Result;
+        //    await _productRepository.RemoveAsync(productEntity);
+        //}
+    }
+}
---
+++ CleanArchMvc.Domain.Tests/ProductUnitTeste1.cs
@@ -0,0 +1,90 @@
+﻿using CleanArchMvc.Domain.Entities;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace CleanArchMvc.Domain.Tests
+{
+    public class ProductUnitTeste1
+    {
+        [Fact(DisplayName = "Create product With Valid State")]
+        public void CreateProduct_WithValidParameters_ResultObjectValidState()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m,
+                99, "product image");
+            action.Should()
+                .NotThrow<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Create Product With Negative Id Value")]
+        public void CreateProduct_NegativeIdValue_DomainExceptionInvalidID()
+        {
+            Action action = () => new Product(-1, "Product Name", "Product Description", 9.99m,
+                99, "product image");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Id value.");
+        }
+
+        [Fact(DisplayName = "Create Product With Short Name Value")]
+        public void CreateProduct_ShortNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Product(1, "Pr", "Product Description", 9.99m, 99, "product image");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid name, too short, minimum 3 characters.");
+        }
+
+        [Fact(DisplayName = "Create Product With Invalid Price Value")]
+        public void CreateProduct_InvalidPriceValue_DomainExceptionShortName()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", -9.99m, 99, "product image");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid price value.");
+        }
+
+        [Fact(DisplayName = "Create Product With Long Image Name")]
+        public void CreateProduct_LongImageNameValue_DomainExceptionShortName()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "product image toooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo loooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooonnnnnnnnnngggggggggggggggggggggggggggggggggggg");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid image name, too long, maximum 250 characters.");
+        }
+
+        [Fact(DisplayName = "Create Product With Null Image Name")]
+        public void CreateProduct_NullImageValue_NoDomainException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, null);
+            action.Should()
+                .NotThrow<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Fact(DisplayName = "Create Product With Null Image Name Reference Exeception")]
+        public void CreateProduct_NullImageValue_NoNullRereferenceException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, null);
+            action.Should()
+                .NotThrow<NullReferenceException>();
+        }
+
+        [Fact(DisplayName = "Create Product With Empty Image Name")]
+        public void CreateProduct_EmptyImageValue_NoDomainException()
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, 99, "");
+            action.Should()
+                .NotThrow<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+        }
+
+        [Theory]
+        [InlineData(-5)]
+        public void CreateProduct_InvalidStockValue_DomainExceptionShortName(int value)
+        {
+            Action action = () => new Product(1, "Product Name", "Product Description", 9.99m, value, "product image");
+            action.Should()
+                .Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid stock value.");
+        }
+    }
+}

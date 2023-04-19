569cfa311853c6b5a84f6c5fd9252230ad3f2519
Bruno Bordin <bruno.bordin@hammerconsult.com.br>
2023-04-19 14:22:47 -0300

    removendo duplicação de métodos

--- CleanArchMvc.Infra.Data/Repositories/ProductRepository.cs
+++ CleanArchMvc.Infra.Data/Repositories/ProductRepository.cs
@@ -25,15 +25,17 @@ namespace CleanArchMvc.Infra.Data.Repositories
 
         public async Task<Product> GetByIdAsync(int? id)
         {
-            return await _productContext.Products.FindAsync(id);
-        }
-
-        public async Task<Product> GetProductCategoryAsync(int? id)
-        {
-            // include é um eager load
+            //return await _productContext.Products.FindAsync(id);
             return await _productContext.Products.Include(x => x.Category)
                 .SingleOrDefaultAsync(p => p.Id == id);
         }
+
+        //public async Task<Product> GetProductCategoryAsync(int? id)
+        //{
+        //    // include é um eager load
+        //    return await _productContext.Products.Include(x => x.Category)
+        //        .SingleOrDefaultAsync(p => p.Id == id);
+        //}
 
         public async Task<IEnumerable<Product>> GetProductsAsync()
         {

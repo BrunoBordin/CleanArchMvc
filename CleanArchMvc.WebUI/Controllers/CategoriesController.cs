c680f48c191c14dd1ad4c127f4e121d3d4d9e8a7
Bruno Bordin <bruno.bordin@hammerconsult.com.br>
2023-04-24 20:16:14 -0300

    Editar uma categoria

--- CleanArchMvc.WebUI/Controllers/CategoriesController.cs
+++ CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -27,12 +27,41 @@ namespace CleanArchMvc.WebUI.Controllers
             return View();
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var categoryDTO = await _categoryService.GetById(id);
+            if (categoryDTO == null) return NotFound();
+
+            return View(categoryDTO);
+        }
+
         [HttpPost]
         public async Task<IActionResult> Create(CategoryDTO category)
         {
             if (ModelState.IsValid)
             {
                 await _categoryService.Add(category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(CategoryDTO category)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _categoryService.Update(category);
+                }
+                catch (System.Exception)
+                {
+                    throw;
+                }
                 return RedirectToAction(nameof(Index));
             }
             return View(category);

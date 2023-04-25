a170f926fe19b3a5d90a61fb0b5b53abd50d45a4
Bruno Bordin <bruno.bordin@hammerconsult.com.br>
2023-04-24 21:09:24 -0300

    Exibir detalhes de um produto

--- CleanArchMvc.WebUI/Controllers/ProductsController.cs
+++ CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -1,18 +1,22 @@
 ﻿using CleanArchMvc.Application.DTOs;
 using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.WebUI.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly IWebHostEnvironment _environment;
         private readonly IProductService _productService;
         private readonly ICategoryService _categoryService;
 
-        public ProductsController(IProductService productService, ICategoryService categoryService)
+        public ProductsController(IWebHostEnvironment environment, IProductService productService, ICategoryService categoryService)
         {
+            _environment = environment;
             _productService = productService;
             _categoryService=categoryService;
         }
@@ -56,6 +60,21 @@ namespace CleanArchMvc.WebUI.Controllers
 
             var productDto = await _productService.GetById(id);
             if (productDto == null) return NotFound();
+
+            return View(productDto);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+            var productDto = await _productService.GetById(id);
+
+            if (productDto == null) return NotFound();
+
+            var wwwroot = _environment.WebRootPath;
+            var image = Path.Combine(wwwroot, "images\\" + productDto.Image);
+            var exists = System.IO.File.Exists(image);
+            ViewBag.ImageExist = exists;
 
             return View(productDto);
         }
